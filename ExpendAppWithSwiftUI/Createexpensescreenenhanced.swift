f0745f15b1cf6cd4e19e984da82db40e8ede9372
Visalroth <chouvisalroth9999@gmail.com>
2026-02-06 08:08:30 +0700

    first initial project

---
+++ ExpendAppWithSwiftUI/Createexpensescreenenhanced.swift
@@ -0,0 +1,18 @@
+//
+//  Createexpensescreenenhanced.swift
+//  ExpendAppWithSwiftUI
+//
+//  Created by Visalroth on 5/2/26.
+//
+
+import SwiftUI
+
+struct Createexpensescreenenhanced: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Createexpensescreenenhanced()
+}
